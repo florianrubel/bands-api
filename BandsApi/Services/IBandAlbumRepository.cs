e0f30798d991f31f9c72bd279c1f28e37c95aa44
Florian Rubel <florianrubel@display-awesome.com>
2020-01-15 00:08:11 +0100

    initial commit

---
+++ BandsApi/Services/IBandAlbumRepository.cs
@@ -0,0 +1,26 @@
+﻿using BandsApi.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace BandsApi.Services
+{
+    public interface IBandAlbumRepository
+    {
+        IEnumerable<Album> GetAlbums(Guid bandId);
+        Album GetAlbum(Guid bandId, Guid albumId);
+        void AddAlbum(Guid bandId, Album album);
+        void UpdateAlbum(Album album);
+        void DeleteAlbum(Album album);
+
+        IEnumerable<Band> GetBands();
+        Band GetBand(Guid bandId);
+        IEnumerable<Band> GetBands(IEnumerable<Guid> bandIds);
+        void AddBand(Band band);
+        void UpdateBand(Band band);
+        void DeleteBand(Band band);
+
+        bool BandExists(Guid bandId);
+        bool AlbumExists(Guid albumId);
+        bool Save();
+    }
+}
