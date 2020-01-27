1c7936e1d21cfe736b8943e8bcaa3687a2a0c74e
Florian Rubel <florianrubel@display-awesome.com>
2020-01-27 23:49:05 +0100

    trying out swagger

--- BandsApi/Startup.cs
+++ BandsApi/Startup.cs
@@ -8,10 +8,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Pomelo.EntityFrameworkCore.MySql.Storage;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace BandsApi
 {
@@ -58,11 +61,31 @@ namespace BandsApi
 
             services.AddScoped<IPropertyMappingService, PropertyMappingService>();
             services.AddScoped<IPropertyValidationService, PropertyValidationService>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Bands Api Doc with Swashbuckle", Version = "v1" });
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bands Api Doc with Swashbuckle");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
